e7a73ddd43be7bbeac60f2b6e912d23a1fca76eb
vijaysony328-lgtm <vijaysony328@gmail.com>
2026-05-26 23:55:38 +0530

    Simplify azurerm_subnet resource definition
    Removed subnet configuration details from azurerm_subnet resource.

--- subnet.tf
+++ subnet.tf
@@ -1,6 +1,1 @@
-resource "azurerm_subnet" "this" {
-  name                 = var.subnet_name
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.this.name
-  address_prefixes     = var.subnet_address_prefixes
-}
+resource "azurerm_subnet" "this" {}

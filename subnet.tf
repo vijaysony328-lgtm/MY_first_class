110474c8fa96628019933ffe493413a9fcefc468
vijaysony328-lgtm <vijaysony328@gmail.com>
2026-05-27 23:28:58 +0530

    Delete subnet.tf

--- subnet.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_subnet" "this" {
-  name                 = var.subnet_name
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.this.name
-  address_prefixes     = var.subnet_address_prefixes
-}
